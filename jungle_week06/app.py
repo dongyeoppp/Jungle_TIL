5299eeccc9b9bd95938f62629cb875ff976898d9
dongyeoppp <ds06041@gmail.com>
2024-04-28 00:48:56 +0900

    2024-04-28

--- jungle_week06/app.py
+++ jungle_week06/app.py
@@ -25,10 +25,52 @@
 
     
   
+# 출석체크   
 
-                
+# import sys 
 
-        
+# n , k , q , m = map(int,sys.stdin.readline().split())
+# ran = []
+# sleep = list(map(int,sys.stdin.readline().split()))  
+
+# check = list(map(int,sys.stdin.readline().split()))
+# for i in range(m):
+#     s,e = map(int,sys.stdin.readline().split())
+#     ran.append((s,e))
+
+# result= 0
+# for j in ran:
+#     new = j[1]-j[0]+1
+#     visited= [False]*(n+3)
+#     for h in check:
+#         save = h
+#         count=2
+#         if h in sleep:
+#             continue
+#         else:
+#             while  h <= j[1]:
+#                 if h not in sleep and not visited[h]:
+#                     if j[0]<=h:
+#                         new-=1 
+#                         visited[h] = True
+#                 h = save *count
+#                 count+=1
+#     print(new)
+    
+import sys 
+
+n , k , q , m = map(int,sys.stdin.readline().split())
+ran = []
+sleep = list(map(int,sys.stdin.readline().split()))  
+
+check = list(map(int,sys.stdin.readline().split()))
+for i in range(m):
+    s,e = map(int,sys.stdin.readline().split())
+    ran.append((s,e))
+
+result= 0
+
+for h in check:
 
                 
 
