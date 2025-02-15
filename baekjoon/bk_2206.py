e3e09382cf755e25872d5f8e3a444889fb0e3c52
dongyeop <ds06041@gmail.com>
2025-02-15 21:41:06 +0900

    백준 2206번 정답 문제 풀이

--- baekjoon/bk_2206.py
+++ baekjoon/bk_2206.py
@@ -1,5 +1,5 @@
 # 벽 부수고 이동하기   
-
+# bfs를 사용하여 풀이
 
 import sys
 from collections import deque
@@ -14,29 +14,35 @@ for i in range(n):
 
 dy = [1,-1,0,0]
 dx = [0,0,1,-1]
-visited = [[float('inf')]*m for _ in range(n)]
+# 3차원 배열을 사용 
+# visited[row][col][0] -> 벽을 부시지 않은 경로
+# visited[row][col][1] -> 벽을 부신 경로 
+visited = [[[0]*2 for _ in range(m)] for _ in range(n)]
 
 def bfs(row,col,count):
     que = deque()
-    visited[row][col] = 1
+    visited[row][col][count] = 1
     que.append((row,col,count))
     while que:
-        rerow, recol, cnt = que.popleft()
+        rerow,recol,recount = que.popleft()
+        # 목표 위치에 도달 
+        if rerow == n-1 and recol == m-1:
+            return visited[rerow][recol][recount]
         for i in range(4):
             x = dx[i] + rerow
             y = dy[i] + recol
-            if 0<=x <n and 0<= y <m and visited[rerow][recol]+1 < visited[x][y]:
-                if cnt == 0 or new[x][y] == "0":
-                    visited[x][y] = visited[rerow][recol] + 1
-                    if new[x][y] == "1":
-                        cnt +=1
-                    que.append((x,y,cnt))
-                    
-    return
+            if 0<= x < n and 0 <= y < m :
+                # 벽을 만났지만 벽을 부수지 않은 경로일 경우  
+                if new[x][y] == "1" and recount == 0:
+                    visited[x][y][1] = visited[rerow][recol][0] + 1
+                    que.append((x,y,1))
+ 
+                # 벽이 안니고 지나치지 않은 경로일 경우  
+                elif new[x][y] == "0" and visited[x][y][recount] == 0:
+                    visited[x][y][recount] = visited[rerow][recol][recount] + 1
+                    que.append((x,y,recount))
+            
+    return -1
 
-bfs(0,0,0)
-print(visited)
-if visited[n-1][m-1] == float('inf'):   
-    print(-1)
-else:
-    print(visited[n-1][m-1])
+print(bfs(0,0,0))
+
